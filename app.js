a6825af4c5fc717c31fe8109ca50b847b95b729d
Dalton de Oliveira Silva <daltonos@users.noreply.github.com>
2019-01-27 13:31:57 -0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const http = require('http')
+const app = express()
+const port = 3000
+const {google} = require('googleapis');
+const youtube = google.youtube('v3');
+
+var arrayOfIds = [];
+var finalRes;
+var playlistId = '';
+
+
+function getPageFromPlaylist(nextToken) {
+    var requestObject = {
+      key: [PLACE_YOUR_YOUTUBE_APP_KEY_HERE_FROM_GOOGLE_CONSOLE],
+      part: 'id,snippet',
+      playlistId: playlistId,
+      maxResult: 10,
+    }
+    
+    if (nextToken != 'start') requestObject.pageToken = nextToken;
+
+    youtube.playlistItems.list(requestObject , (err, results) => {
+
+        if(err) return;
+        
+        //accumulate ids
+        results.data.items.forEach(function(video){
+            arrayOfIds.push(video.snippet.resourceId.videoId);
+        });
+        
+        if(results && results.data && results.data.nextPageToken){
+            getPageFromPlaylist(results.data.nextPageToken);
+        } else {
+            finalRes.send(arrayOfIds[Math.floor(Math.random()*arrayOfIds.length)]);
+        }
+    });
+}
+
+app.get('/', (req, res) => {
+    finalRes = res;
+    playlistId = req.query.playlist;
+    getPageFromPlaylist('start');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
